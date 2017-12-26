f7b3bf96a7246682b51417ab7afd1ae0ba0aa4ac
baurine <2008.hbl@gmail.com>
2017-12-26 18:13:51 +0800

    MISC - Implement add space between Chinese and English by pangu

--- extension.js
+++ extension.js
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const pangu = require('pangu');
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -13,11 +14,33 @@ function activate(context) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', function () {
+    let disposable = vscode.commands.registerCommand('extension.addSpace', function () {
         // The code you place here will be executed every time your command is executed
 
         // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+        // vscode.window.showInformationMessage('Hello World!');
+
+        const editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            return;  // No open text editor
+        }
+
+        editor.edit(builder => {
+            const document = editor.document
+            const lineCount = document.lineCount
+
+            for (let i=0; i<lineCount; i++) {
+                const textLine = document.lineAt(i)
+                const oriTrimText = textLine.text.trimRight()
+
+                if (oriTrimText.length === 0) {
+                    builder.replace(textLine.range, '')
+                } else {
+                    const panguText = pangu.spacing(oriTrimText)
+                    builder.replace(textLine.range, panguText)
+                }
+            }
+        })
     });
 
     context.subscriptions.push(disposable);
